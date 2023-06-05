323563fc75fe1a91d08a0a0ff7b476b8005c531d
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-05 00:42:45 -0500

    [Backend] CRUD working

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,14 +1,14 @@
-import { exampleRouter } from "~/server/api/routers/example";
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter } from "./trpc";
+import { postRouter } from "./routers/post";
+import { userRouter } from "./routers/user";
+import { exampleRouter } from '../api/routers/example';
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
+
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  post: postRouter,
+  user: userRouter,
+  //example: exampleRouter
 });
 
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -22,6 +22,32 @@ export const exampleRouter = createTRPCRouter({
     return ctx.prisma.user.findMany();
   }),
 
+  getUser: publicProcedure.query(({ ctx }) => {
+    const user = ctx.prisma.user.findUnique; // Suponiendo que tienes el ID del usuario en la sesión
+  
+    return ctx.prisma.user.findUnique({
+      where: {
+        id: user.name,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        posts: true,
+      },
+    });
+  }),
+
+  
+  getfullUser: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findMany({
+      include: {
+        posts: true,
+      },
+    });
+  }),
+  
+
   getOne: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(({ input, ctx }) => {
